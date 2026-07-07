b6474313d66d2f3b2e470f5427d94c73764bd407
mauriciofree <mauricio@freeline.inf.br>
2026-07-07 19:37:36 -0300

    feat: funcão de confirmar operação (futura exclusão/devolução)

--- src/utils/input.ts
+++ src/utils/input.ts
@@ -9,6 +9,13 @@ export async function perguntar(mensagem: string): Promise<string> {
   return resposta.trim();
 }
 
+export async function confirmar(mensagem: string): Promise<boolean> {
+  const resposta = await perguntar(`${mensagem} (s/n): `);
+  const respostaNormalizada = resposta.toLowerCase();
+
+  return respostaNormalizada === "s" || respostaNormalizada === "sim";
+}
+
 export async function pausar(): Promise<void> {
   await perguntar("\nPressione Enter para continuar...");
 }
